74fab601b65eb3fe313310827661cec9823124d0
Charles Robertson <admin@cocohost.co>
2019-11-11 16:12:10 -0600

    Image shrink and grow functions work

--- dist/imagegrow.js
+++ dist/imagegrow.js
@@ -8,12 +8,12 @@ const body = document.querySelector('body');
 
 // Convert duration custom property to milliseconds
 const duration = 1000 * Number(getComputedStyle(document.documentElement).getPropertyValue('--duration').slice(0, -1));
-console.log(duration);
 
 // Functions
-
 // @params img as Node
 function grow(img) {
+  img.style.transition = 'transform .2s ease';
+
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
 
@@ -40,9 +40,9 @@ function grow(img) {
     growWidth = growHeight / imgAspect;
   }
   
-  const growShiftY = (-imgTop + (windowHeight - imgHeight) / 2).toFixed(2);
-  const growShiftX = (-imgLeft + (windowWidth - imgWidth) / 2).toFixed(2);
-  const growScale = (growWidth / imgWidth).toFixed(2);
+  const growShiftY = (-imgTop + (windowHeight - imgHeight) / 2);
+  const growShiftX = (-imgLeft + (windowWidth - imgWidth) / 2);
+  const growScale = (growWidth / imgWidth);
   
   img.style.setProperty('--img-top', imgTop + 'px');
   img.style.setProperty('--img-left', imgLeft + 'px');
@@ -56,16 +56,42 @@ function grow(img) {
 function shrink(img) {
   const figure = img.parentNode;
 
-  const windowWidth = window.innerWidth;
-  const windowHeight = window.innerHeight;
-
+  const imgTop = Number(getComputedStyle(img).getPropertyValue('--img-top').slice(0, -2));
+  
+  // const imgLeft = Number(getComputedStyle(img).getPropertyValue('--img-left').slice(0, -2));
+  
   const figureOffset = figure.getBoundingClientRect();
   const figureTop = figureOffset.top;
-  const figureLeft = figureOffset.left;
-  const figureWidth = figureOffset.width;
-  const figureHeight = figureOffset.height;
-  
 
+  const growShiftX = 0;
+  const growShiftY = figureTop - imgTop;
+  const growScale = 1;
+
+  console.log('scrollY:', window.scrollY);
+  console.log('imgTop:', imgTop);
+  console.log('figureTop:', figureTop);
+  console.log('growShiftY:', growShiftY);
+
+  img.style.setProperty('--grow-shift-x', growShiftX + 'px');
+  img.style.setProperty('--grow-shift-y', growShiftY + 'px');
+  img.style.setProperty('--grow-scale', growScale);
+
+  // const windowWidth = window.innerWidth;
+  // const windowHeight = window.innerHeight;
+
+  // const imgTop = img.getBoundingClientRect().top;
+
+  // const shrinkLeft = shrinkOffset.left;
+  // const shrinkWidth = shrinkOffset.width;
+  // const shrinkHeight = shrinkOffset.height;
+
+  // const shrinkShiftY = -window.scrollY + shrinkOffset.top;
+  // const shrinkShiftX
+  
+  setTimeout(function () {
+    img.style.transition='transform 0s linear';
+    img.classList.remove('grow')
+  }, duration);
 }
 
 function toggleGrow() {
@@ -73,26 +99,13 @@ function toggleGrow() {
 
   if (this.classList.contains('grow')) {
     shrink(img);
-
-    this.classList.remove('grow');
   } else {
     grow(img);
   }
 }
 
-function shrinkAll() {
-  console.log('shrinkAll')
-  imgs.forEach(function(img) {
-    if (img.classList.contains('grow')) {
-      img.classList.remove('grow');  
-    }             
-  });
-  
-  document.removeEventListener('click', shrinkAll);
-}
-
 // Event Listeners
-imgs.forEach(function(figure) {
-   figure.addEventListener('click', toggleGrow);              
+imgs.forEach(function(img) {
+   img.addEventListener('click', toggleGrow);              
 });
 
