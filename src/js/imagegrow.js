328eca3426391e87fbba67d0daeae3a85aa7c1db
Charles Robertson <admin@cocohost.co>
2019-11-11 10:22:58 -0600

    Made grow() its own function

--- src/js/imagegrow.js
+++ src/js/imagegrow.js
@@ -7,53 +7,55 @@ const imgs = document.querySelectorAll('.text-block__figure > img');
 const body = document.querySelector('body');
 
 // Functions
+
+// @params img as Node
+function grow(img) {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  // Set the margin around enlarged image as percent of window width
+  const windowMargin = .15 * windowWidth;
+
+  const imgOffset = img.getBoundingClientRect();
+  const imgTop = imgOffset.top;
+  const imgLeft = imgOffset.left;
+  const imgWidth = imgOffset.width;
+  const imgHeight = imgOffset.height;
+  
+  const windowAspect = windowHeight / windowWidth;  
+  const imgAspect = imgHeight / imgWidth;
+  
+  let growWidth, growHeight;
+
+  // Chooses whether to use window width or height as the constraining dimension
+  if (imgAspect <= windowAspect) {
+    growWidth = windowWidth - (windowMargin * 2);
+    growHeight = growWidth * imgAspect;
+  } else {
+    growHeight = windowHeight - (windowMargin * 2);
+    growWidth = growHeight / imgAspect;
+  }
+  
+  const growY = -imgTop + (windowHeight - imgHeight) / 2;
+  const growScale = growWidth / imgWidth;
+  
+  img.style.setProperty('--img-top', imgTop + 'px');
+  img.style.setProperty('--img-left', imgLeft + 'px');
+  img.style.setProperty('--grow-scale', growScale);
+  img.style.setProperty('--grow-y', growY + 'px');
+
+  img.classList.add('grow');
+}
+
 function toggleGrow() {
+  const img = this;
 
   if (this.classList.contains('grow')) {
     
     this.classList.remove('grow');
     // document.removeEventListener('click', shrinkAll);
   } else {
-    // const windowMargin = Number(getComputedStyle(document.documentElement).getPropertyValue('--window-margin'));
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    const windowMargin = .15 * windowWidth;
-
-    const imgOffset = this.getBoundingClientRect();
-    const imgTop = imgOffset.top;
-    const imgLeft = imgOffset.left;
-    const imgWidth = imgOffset.width;
-    const imgHeight = imgOffset.height;
-    
-    const windowAspect = windowHeight / windowWidth;  
-    const imgAspect = imgHeight / imgWidth;
-    
-    let growWidth, growHeight;
-
-    if (imgAspect <= windowAspect) {
-      growWidth = windowWidth - 2 * windowMargin;
-      growHeight = growWidth * imgAspect;
-    } else {
-      growHeight = windowHeight - 2 * windowMargin;
-      growWidth = growHeight / imgAspect;
-    }
-    
-    const growY = -imgTop + (windowHeight - imgHeight) / 2;
-    // const growLeft = windowMargin + (windowWidth - growWidth) / 2;
-    const growScale = growWidth / imgWidth;
-    
-    this.style.setProperty('--img-top', imgTop + 'px');
-    this.style.setProperty('--img-left', imgLeft + 'px');
-    this.style.setProperty('--grow-scale', growScale);
-    this.style.setProperty('--grow-y', growY + 'px');
-
-    console.log(growWidth, growHeight)
-    // this.style.setProperty('--grow-', imgLeft);
-    // this.style.setProperty('--img-left', imgLeft);
-    
-    this.classList.add('grow');
-    // document.addEventListener('click', shrinkAll);
+    grow(img);
   }
 }
 
