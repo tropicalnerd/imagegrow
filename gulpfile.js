3135bb6e62204a2913f6e49da91e2fa7ac57d39e
Charles Robertson <admin@cocohost.co>
2019-11-11 22:19:25 -0600

    Add responsive images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,55 @@
 const gulp = require('gulp');
+const changed = require('gulp-changed');
+const imageResize = require('gulp-image-resize');
+const imagemin = require('gulp-imagemin');
+const imageminMozJpeg = require('imagemin-mozjpeg');
+const imageminWebp = require('imagemin-webp');
+const rename = require('gulp-rename');
 const pug = require('gulp-pug');
 const stylus = require('gulp-stylus');
 const browsersync = require('browser-sync');
+
+// Image resize and compress
+function jpg(cb) {
+  [180, 270, 360, 720, 1080].forEach(function (size) {
+    gulp.src('src/images/*.{jpg,jpeg,png}')
+      .pipe(changed('dist/images'))
+      .pipe(imageResize({ height: size, format: jpg }))
+      .pipe(rename(function (path) {
+        path.basename = `${path.basename}_${size}`;
+        path.extname = '.jpg';
+      }))
+      .pipe(imagemin([
+        imageminMozJpeg({
+          quality: 50,
+          progressive: true
+        })
+      ], {verbose: true}))
+      .pipe(gulp.dest('dist/images'))
+  });
+  cb();
+}
+gulp.task('jpg', jpg);
+
+function webp(cb) {
+  [180, 270, 360, 720, 1080].forEach(function (size) {
+    gulp.src('src/images/*.{jpg,jpeg,png}')
+      .pipe(changed('dist/images'))
+      .pipe(imageResize({ height: size }))
+      .pipe(imagemin([
+        imageminWebp({
+          quality: 50,
+        })
+      ], {verbose: true}))
+      .pipe(rename(function (path) {
+        path.basename = `${path.basename}_${size}`;
+        path.extname = '.webp';
+      }))
+      .pipe(gulp.dest('dist/images'))
+  });
+  cb();
+}
+gulp.task('webp', webp);
 
 gulp.task('pug', function buildHTML() {
   return gulp.src('src/pug/index.pug')
@@ -28,6 +76,7 @@ gulp.task('serve', function() {
     server: 'dist'
   });
 
+  gulp.watch(['src/images/*.{jpg, jpeg}'], gulp.series('images'));
   gulp.watch('src/pug/*.pug', gulp.series('pug'));
   gulp.watch('src/stylus/*.stylus', gulp.series('stylus'));
   gulp.watch('src/js/*.js', gulp.series('js'));
@@ -35,4 +84,5 @@ gulp.task('serve', function() {
   gulp.watch(['dist/index.html', 'dist/*.js']).on('change', browsersync.reload);
 });
 
+gulp.task('images', gulp.parallel('jpg', 'webp'));
 gulp.task('default', gulp.series('serve'));
